a8ba8ef666f55fddda5056c7a1ecec0d5df0ac40
Anastasiia Sedova <sedovanastasiia@gmail.com>
2026-05-06 13:58:01 +0200

    changes

--- macroII.jl
+++ macroII.jl
@@ -1,7 +1,12 @@
 using CSV, DataFrames
 
-df = CSV.read("/Users/asik/Downloads/99911-0012_de.csv", DataFrame, delim=';')
-df_rate = CSV.read("/Users/asik/Downloads/IRSTCI01DEM156N.csv", DataFrame, delim=',')
+df_rate = CSV.read("IRSTCI01DEM156N.csv", DataFrame, delim=',')
+
+df = CSV.read("99911-0012_de.csv", DataFrame, delim=';')
 first(df, 16)
 size(df)
 delete!(df, [1:3; 10:16]) 
+delete!(df, [2]) 
+rename!(df, "Tabelle: 99911-0012" => "Variable")
+df.Label = ["Year", "GDP", "GDP per capita", "Growth", "Inflation"]
+select!(df, Not(names(df, all.(ismissing, eachcol(df)))))
